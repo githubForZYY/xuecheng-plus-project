65022563ce72ea61e811621ef93672011ac37407
zyy <2681236734@qq.com>
2023-05-22 21:23:42 +0800

    完成修改课程功能

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,12 +4,14 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.AddCourseDto;
 import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.EditCourseDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -53,5 +55,13 @@ public class CourseBaseInfoController {
     public CourseBaseInfoDto getCourseBaseInfoById(@PathVariable Long courseId){
         CourseBaseInfoDto dto=courseBaseInfoService.getCourseBaseInfo(courseId);
         return dto;
+    }
+    @ApiOperation("修改课程信息")
+    @PutMapping("/course")
+    public CourseBaseInfoDto updateCourseBase(@RequestBody @Validated EditCourseDto dto){
+        //机构id
+        Long companyId = 1232141425L;
+        CourseBaseInfoDto courseBaseInfoDto=courseBaseInfoService.updateCourseBase(companyId,dto);
+        return  courseBaseInfoDto;
     }
 }
