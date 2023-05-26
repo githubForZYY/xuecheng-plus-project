33ff688e23ec4ad9d1872c90e35cd8e340079515
zyy <2681236734@qq.com>
2023-05-26 23:26:23 +0800

    完成媒资模块工程的创建

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -0,0 +1,25 @@
+package com.xuecheng.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.model.po.Teachplan;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 课程计划 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface TeachplanMapper extends BaseMapper<Teachplan> {
+
+
+    /**
+     * @decription 通过课程id查询章节内容树结构
+     * @param courseId 课程id
+     * @return
+     */
+    List<TeachplanDto> selectTreeNodes(long courseId);
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,55 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.EditCourseDto;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 课程信息管理接口
+ * @date 2023/2/12 10:14
+ */
+public interface CourseBaseInfoService {
+
+    /**
+     * 课程分页查询
+     * @param pageParams 分页查询参数
+     * @param courseParamsDto 查询条件
+     * @return 查询结果
+     */
+     PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto courseParamsDto);
+
+    /**
+     * 新增课程
+     * @param companyId 机构id
+     * @param addCourseDto 课程信息
+     * @return 课程详细信息
+     */
+     CourseBaseInfoDto createCourseBase(Long companyId,AddCourseDto addCourseDto);
+
+    /**
+     * 根据id查询课程基本信息
+     * @param courseId 课程id
+     * @return 课程基本信息
+     */
+    CourseBaseInfoDto getCourseBaseInfo(Long courseId);
+
+    /**
+     *
+     * @param companyId 机构id
+     * @param dto 前端发送的需要更改的课程信息
+     * @return
+     */
+     CourseBaseInfoDto updateCourseBase(Long companyId, EditCourseDto dto);
+
+    /**
+     * 删除课程
+     * @param courseId 课程id
+     */
+    void removeCourse(long courseId);
+}
