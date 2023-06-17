42b7fd2c3285180b3eb73ac3ec6885d33ac1e776
zyy <2681236734@qq.com>
2023-06-17 23:26:09 +0800

    完成用户认证功能开发，导入验证码工程

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -8,10 +8,12 @@ import com.xuecheng.content.model.dto.EditCourseDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
+import com.xuecheng.content.util.SecurityUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -54,8 +56,12 @@ public class CourseBaseInfoController {
     @ApiOperation("根据课程id查询课程基础信息")
     @GetMapping("course/{courseId}")
     public CourseBaseInfoDto getCourseBaseInfoById(@PathVariable Long courseId){
-        CourseBaseInfoDto dto=courseBaseInfoService.getCourseBaseInfo(courseId);
-        return dto;
+        //取出当前用户身份
+//    Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        SecurityUtil.XcUser user = SecurityUtil.getUser();
+        System.out.println(user);
+
+        return courseBaseInfoService.getCourseBaseInfo(courseId);
     }
     @ApiOperation("修改课程信息")
     @PutMapping("/course")
